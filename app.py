43876d587de0a92d8f659bad33f0f44561fffc86
kxrai <alicia.2005.loi@gmail.com>
2024-05-13 11:42:10 -0400

    Hello World API Complete

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+app = Flask(__name__) # Needed so Flask knows where to look for resources
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
